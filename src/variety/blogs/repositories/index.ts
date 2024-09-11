d14106c92704940edaf19f0db8d5d5421e7e1349
Vladimir NG <potd3@ukr.net>
2024-09-12 00:32:21 +0300

    delete Repositoty Array

--- src/variety/blogs/repositories/index.ts
+++
@@ -1,6 +0,0 @@
-//import  {blogRepositoryArray} from "./blogRepositoryArray";
-import { blogRepositoryMongo } from "./blogRepositoryMongo";
-
-//export const blogRepository = blogRepositoryArray;
-
-export const blogRepository = blogRepositoryMongo;
--- src/variety/posts/repositories/index.ts
+++
@@ -1,6 +0,0 @@
-//import  {postRepositoryArray} from "./postRepositoryArray";
-import { postRepositoryMongo } from "./postRepositoryMongo";
-
-//export const postRepository = postRepositoryArray;
-
-export const postRepository = postRepositoryMongo;
