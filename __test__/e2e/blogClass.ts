f0348ab743fa56e2df61db87d1c0e4cf1bd3b118
Vladimir NG <potd3@ukr.net>
2024-09-30 03:40:58 +0300

    test PostBlog

---
+++ __test__/e2e/blogClass.ts
@@ -0,0 +1,305 @@
+import {APIErrorResult, BlogInputModel, BlogViewModel, FieldError, QueryModel} from '../../src/types'
+import {HTTP_STATUSES, URL_PATH} from '../../src/setting'
+import { app } from "../../src/app";
+import request from "supertest";
+import { SortDirection } from 'mongodb';
+import { PostEndPoint } from './postClass';
+
+type InputQuery = {
+    sortBy?: string,
+    sortDirection?: SortDirection,
+    pageNumber?: number,
+    pageSize?: number,
+    searchNameTerm?: string }
+
+export class BlogEndPoint{
+    
+    private blogView: Array<BlogViewModel> = []
+
+    private blogCorrect: Array<BlogInputModel> = []
+    private blogIncorrect: Array<BlogInputModel>
+    private blogErrors: Array<APIErrorResult>
+    private errorName: FieldError
+    private errorDescription: FieldError
+    private errorWeb: FieldError
+    private Auth!: string
+    private posts: PostEndPoint
+
+
+    constructor(){
+
+        this.posts = new PostEndPoint()
+
+        for(let i = 0; i < 10; i++){
+            this.blogCorrect[i]= {
+                name: 'name' + i,                   
+                description: 'string' + i,            
+                websiteUrl:	'https://one.two.com'         
+            }
+        }
+
+        this.blogIncorrect = [
+        {    name: "length16123456789",
+            description: "0123",
+            websiteUrl: "ttps://google123456789012345678901234567890.com"
+            },
+        {   name: '',                   
+            description: 'lenght501012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901',            
+            websiteUrl:	'http://one.two.com'         
+            },
+        {   name: "",
+            description: "0123",
+            websiteUrl: "https://length101googlecom.vfkgjfndjfnvg.gmnfkdlkm.fmnvkkmkm.lekmnjnnnnnnlek.lekmkmkmemkmekmekmek.ekm"
+        }]
+
+        this.errorName = {
+            "message": expect.any(String),
+            "field": "name"
+        }
+        this.errorDescription = {
+            "message": expect.any(String),
+            "field": "description"
+        }    
+        this.errorWeb = {
+            "message": expect.any(String),
+            "field": "websiteUrl"
+        }
+        this.blogErrors = [{
+            "errorsMessages": [this.errorName, this.errorWeb ]
+        },
+        { "errorsMessages": [this.errorName, this.errorDescription, this.errorWeb ]
+        },
+        { "errorsMessages": [this.errorName, this.errorWeb ]
+        }]
+
+        this.onAuthorization()
+    }
+
+    async createItem(i: number = 0, name?: string, description?: string, websiteUrl?: string ){
+        let inName = name ? {name: name} : {}
+        let inDescription = description ? {description: description} : {}
+        let  inWebsiteUrl = websiteUrl ? {websiteUrl: websiteUrl} : {}
+        const postItem = {...this.blogCorrect[i], ...inName, ...inDescription, ...inWebsiteUrl}
+        let createdResponse = await 
+                request(app)
+                .post(URL_PATH.blogs)
+                .set("Authorization", this.Auth)
+                .send(postItem)
+                .expect(HTTP_STATUSES.CREATED_201);
+
+        let createdItem = createdResponse.body;
+        this.blogView.push({...createdItem})
+
+        expect(createdItem).toEqual({
+            id: expect.any(String),
+            name: postItem.name,
+            description: postItem.description,
+            websiteUrl: postItem.websiteUrl,
+            createdAt: expect.any(String),
+            isMembership: false
+        })
+    }
+    async createItemNoAuth(i: number = 0){
+        this.offAuthorization()
+        await request(app)
+            .post(URL_PATH.blogs)
+            .set("Authorization", this.Auth)
+            .send(this.blogCorrect[i])
+            .expect(HTTP_STATUSES.NO_AUTHOR_401);
+        this.onAuthorization()
+
+    }
+    async createBADblog(i:number = 0){
+        let createdResponse = await 
+        request(app)
+        .post(URL_PATH.blogs)
+        .set("Authorization", this.Auth)
+        .send(this.blogIncorrect[i])
+        .expect(HTTP_STATUSES.BAD_REQUEST_400);
+
+        let notCreatedError = createdResponse.body;
+
+        expect(notCreatedError).toEqual(this.blogErrors[i]        )
+    }
+
+
+    async getIDblog(numberInArray: number = 0){
+        let foundResponse = await 
+                request(app)
+                .get(`${URL_PATH.blogs}/${this.blogView[numberInArray].id}`)
+                .set("Authorization", this.Auth)
+                .expect(HTTP_STATUSES.OK_200);
+
+        let foundItem = foundResponse.body;
+        
+        expect(foundItem).toEqual(this.blogView[numberInArray])
+    }
+    async getBlogByBadId(id: string){
+        let foundResponse = await 
+                request(app)
+                .get(`${URL_PATH.blogs}/${id}`)
+                .set("Authorization", this.Auth)
+                .expect(HTTP_STATUSES.NOT_FOUND_404);
+    }
+
+
+    async deleteIDblog(numberInArray: number = 0){
+        await request(app)
+                .delete(`${URL_PATH.blogs}/${this.blogView[numberInArray].id}`)
+                .set("Authorization", this.Auth)
+                .expect(HTTP_STATUSES.NO_CONTENT_204);
+        this.blogView.splice(numberInArray, 1)
+        
+    }
+    async deleteNoBlog(notAuth: boolean = true, id: string =this.blogView[0].id ){
+        // true - not authorization
+        let status = HTTP_STATUSES.NOT_FOUND_404
+        
+        if(notAuth){
+            status = HTTP_STATUSES.NO_AUTHOR_401
+            this.offAuthorization()
+        }
+        await request(app)
+                .delete(`${URL_PATH.blogs}/${id}`)
+                .set("Authorization", this.Auth)
+                .expect(status);
+        this.onAuthorization()
+    }
+
+
+    async getBlogs(inputQuery: InputQuery = {})
+    {
+        let foundResponse = await request(app)
+                .get(URL_PATH.blogs)
+                .set("Authorization", this.Auth)
+                .query(inputQuery)
+                .expect(HTTP_STATUSES.OK_200);
+
+        let foundItems = foundResponse.body
+
+        let outArray = this.blogView
+        if(inputQuery?.searchNameTerm){
+            const regExp = new RegExp(inputQuery.searchNameTerm, 'i')
+            outArray = this.blogView.filter(s => s.name.search(regExp))
+        }
+
+        expect(foundItems).toEqual(this.setPaginator(inputQuery, outArray))
+
+    }
+
+
+    async editItem(inView: number = 0, inCorrect = 3, name?: string, description?: string, websiteUrl?: string ){
+        let inName = name ? {name: name} : {}
+        let inDescription = description ? {description: description} : {}
+        let  inWebsiteUrl = websiteUrl ? {websiteUrl: websiteUrl} : {}
+        const editData = {...this.blogCorrect[inCorrect], ...inName, ...inDescription, ...inWebsiteUrl}
+
+        await request(app)
+                .put(`${URL_PATH.blogs}/${this.blogView[inView].id}`)
+                .set("Authorization", this.Auth)
+                .send(editData)
+                .expect(HTTP_STATUSES.NO_CONTENT_204);
+
+        this.blogView[inView] = {...this.blogView[inView], ...editData}
+
+    }
+
+    async editItemByBadId(id: string ){
+    
+        await request(app)
+                .put(`${URL_PATH.blogs}/${id}`)
+                .set("Authorization", this.Auth)
+                .send(this.blogCorrect[0])
+                .expect(HTTP_STATUSES.NOT_FOUND_404);
+
+    }
+
+    async editItemNoAuth(i: number = 0 ){
+        this.offAuthorization()
+
+        await request(app)
+                .put(`${URL_PATH.blogs}/${this.blogView[i].id}`)
+                .set("Authorization", this.Auth)
+                .send(this.blogCorrect[0])
+                .expect(HTTP_STATUSES.NO_AUTHOR_401);
+        this.onAuthorization()
+    }
+
+
+    async createPost(numberBlogInView: number, numberPostInCorrect: number, title?: string, shortDescription?: string, content?: string ){
+        await this.posts.createItem(numberPostInCorrect, this.blogView[numberBlogInView].id, URL_PATH.blogs + URL_PATH.blogsPost, title, shortDescription, content )
+    }
+
+    async createBadPost(numberBlogInView: number, numberPostInCorrect: number){
+    }
+
+    async createPostBadId(numberBlogInView: number, numberPostInCorrect: number ){
+    }
+
+    async createBadPostWithoutAuth(numberBlogInView: number, numberPostInCorrect: number){
+    }
+
+
+    setPaginator(inputQuery: InputQuery, inArray: Array<BlogViewModel>){
+        let {pageSize = 10, pageNumber} = inputQuery
+        const page = pageNumber ? pageNumber : 1
+        
+        if(inArray.length == 0)
+            return{
+                pagesCount: 0,
+                page: 0,
+                pageSize: pageSize,
+                totalCount: 0,
+                items: []
+            }
+
+        type BlogField = keyof BlogViewModel    
+        const fieldSort: BlogField = (inputQuery.sortBy ? inputQuery.sortBy : 'createdAt') as BlogField
+        const directSort = inputQuery.sortDirection == 'asc' ? 1 : -1   
+        
+        if(fieldSort != 'isMembership')
+            inArray.sort((a,b) => a[fieldSort].localeCompare(b[fieldSort]) * directSort ) 
+        else
+        inArray.sort((a,b) => {
+                            if(a[fieldSort] && !b[fieldSort]) 
+                                return directSort
+                            if(!a[fieldSort] && b[fieldSort]) 
+                                return -directSort
+                            return 0}) 
+        
+        let outArray: Array<BlogViewModel> = inArray.slice(pageSize * (page - 1), pageSize * page)
+        
+        return {
+            pagesCount: Math.ceil(outArray.length / pageSize),
+            page: page,
+            pageSize: pageSize,
+            totalCount: outArray.length,
+            items: outArray
+        }
+    }
+
+    onAuthorization(){
+        this.Auth = "Basic YWRtaW46cXdlcnR5"
+    }
+
+    offAuthorization(){
+        this.Auth = 'hygf gj'
+    }
+
+    getNumberItems(){
+        return this.blogView.length
+    }
+
+    showArrayItems(){
+        console.log(this.blogView)
+    }
+
+    getIdItem(i: number){
+        return this.blogView[i].id 
+    }
+
+    getNameItem(i: number){
+        return this.blogView[i].name 
+    }
+
+}
--- __test__/e2e/post.api.test.ts
+++ __test__/e2e/post.api.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { app } from "../../src/app";
 import {MongoMemoryServer} from 'mongodb-memory-server'
 import {MongoClient} from 'mongodb'
-import { BlogEndPoint } from "./blogClass";
+import { PostEndPoint } from "./postClass";
 
 
 
@@ -11,8 +11,8 @@ let server:  MongoMemoryServer
 let uri : string
 let client: MongoClient
 
-describe('/blogs', () => {
-    const blogTest = new BlogEndPoint()
+describe('/posts', () => {
+    let postTest: PostEndPoint
     beforeAll(async() =>{  // clear db-array
         
         server = await MongoMemoryServer.create({
@@ -25,6 +25,7 @@ describe('/blogs', () => {
         client = new MongoClient(uri)
         await client.connect()
         await request(app).delete('/testing/all-data')
+        postTest = await PostEndPoint.getInstance()
 
     })
 
@@ -34,82 +35,78 @@ describe('/blogs', () => {
 
     
     it('should return 200 and empty array', async () => { // watch all blogs [get.blog]
-        await blogTest.getBlogs()    
+        await postTest.getItems()    
     })
  
     it('should return 201 and created object', async () => { // create the fisrt new blog [post.blog]
-        await blogTest.createItem(0, undefined, "ho-ho-ho");
+        await postTest.createItem(0, undefined, "ho-ho-ho");
     })
     
      it('should return 201 and created object', async () => { // create the second new blog [post.blog]
-        await blogTest.createItem(1, 'top')   
+        await postTest.createItem(1, 'top')   
     })
     
     it('should return 401', async () => {  // create the third new blog without Authorization [post.blog]       
-        await blogTest.createItemNoAuth(1) 
+        await postTest.createItemNoAuth(1) 
     })
 
     it('should return 201 and created object', async () => {  // create the third new blog [post.blog]
     
-        await blogTest.createItem(2);
+        await postTest.createItem(2);
     })
         
     it('shouldn\'t delete blog and return 401', async () => { // delete the third blog  with wrong Authorization [delete.blog]
-        await blogTest.deleteNoBlog(true)
+        await postTest.deleteNoItemOrBadAuth(true)
     })
     
     it('should delete blog and return 204', async () => { // delete the last blog [delete.blog]     
-        await blogTest.deleteIDblog(blogTest.getNumberItems() - 1)
+        await postTest.deleteIDitem(postTest.getNumberItems() - 1)
     })
     
     it("shouldn't delete not exist blog and return 404", async () => { // delete not existing blog [delete.blog]
-        await blogTest.deleteNoBlog(false, '53hrfijy6tghu6ghtrfdeswl')
+        await postTest.deleteNoItemOrBadAuth(false, '53hrfijy6tghu6ghtrfdeswl')
     })
         
       it('should watch not exist blog and return 404', async () => { // watch non exist item of blogs with bad Id [get.blog/hj]
-        await blogTest.getBlogByBadId('gyfygu7')    
+        await postTest.getItemByBadId('gyfygu7')    
     
     })
       
     it('should watch the blog return 200 and object', async () => { // watch the first blog  [get.blog/id]
-        await blogTest.getIDblog(0)
+        await postTest.getItemById(0)
     })
     
     
     it('should return 200 and array of all object', async () => { // watch all blogs
-        await blogTest.getBlogs({sortBy: 'name'})       
+        await postTest.getItems({sortBy: 'title'})       
         })
     
     
     it("shouldn't create a blog with incorrect datas and  return 400 and return object of errors", async () => { // create a bad blog [post.blog]
-        await blogTest.createBADblog(0)
-        await blogTest.createBADblog(1)
-        await blogTest.createBADblog(2)
+        await postTest.createBADitem(0)
+        await postTest.createBADitem(1)
+        await postTest.createBADitem(2)
     })
     
     it('should edit the fisrt blog return 204', async () => { // edit the fisrt blog [put.blog]
     
-      await blogTest.editItem(0, 2, 'editName', 'anything')
-
+        await postTest.editItem(1, 4, 'editedTitle', 'anything')
     })
 
+    
     it('shouldn\'t edit the not exist blog and return 404', async () => { // edit the not exist blog [put.blog]
 
-        await blogTest.editItemByBadId('f4e6yerer6')
+        await postTest.editItemByBadId('f4e6yerer6')
     })
 
     it('shouldn\'t edit the exist blog without authorization and return 401', async () => { // edit the not exist blog [put.blog]
 
-        await blogTest.editItemNoAuth(1)
+        await postTest.editItemNoAuth(0)
     })
 
-    it('should create Post for Blogs, return 201 and created object', async () => {  // create the third new blog [post.blog]
-    
-        await blogTest.createPost(1, 5, 'postBlog');
-    })
     it('should return 200 and array of all object', async () => { // watch all blogs
     
-        await blogTest.getBlogs()    
+        await postTest.getItems()    
     })
 
 })   
