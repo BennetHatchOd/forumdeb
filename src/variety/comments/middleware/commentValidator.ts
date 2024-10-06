a0f8abc9155e8e24886d8dec0277e2ca5130cf69
Vladimir NG <potd3@ukr.net>
2024-10-06 19:32:40 +0300

    fix validation comment

--- src/variety/comments/middleware/commentValidator.ts
+++ src/variety/comments/middleware/commentValidator.ts
@@ -1,7 +1,7 @@
 import {body} from 'express-validator'
 
 export const contentValidator = body('content').trim()
-                                         .isLength({min: 20, max: 30})
+                                         .isLength({min: 20, max: 300})
                                          .withMessage("Not correct content's length")
 
 
