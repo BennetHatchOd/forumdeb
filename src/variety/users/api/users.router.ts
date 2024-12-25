5be59915e256b48de77085b20379a41103485dd8
Vladimir NG <potd3@ukr.net>
2024-12-25 12:12:48 +0200

    fix

--- src/variety/users/api/users.router.ts
+++ src/variety/users/api/users.router.ts
@@ -2,11 +2,6 @@ import {Router} from 'express';
 import { authorizatorAdmin } from '../../../midlleware/authorization';
 import {checkInputValidation} from '../../../midlleware/checkInputValidators'
 import { userValidator } from './middleware/user.validator';
-import { UserControllers} from './user.controllers';
-import { UserService } from '../application/user.service';
-import { UserRepository } from '../repositories/user.repository';
-import { UserQueryRepository } from '../repositories/user.query.repository';
-import { AuthRepository } from '../../auth/authRepository';
 import { userControllers } from '../../../instances';
 
 export const usersRouter = Router({});
