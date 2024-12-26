b8de173fa2a50ec7de169e94680ba3504069fee6
Vladimir NG <potd3@ukr.net>
2024-12-26 12:28:52 +0200

    fill instance.ts

--- src/instances.ts
+++ src/instances.ts
@@ -6,6 +6,30 @@ import { UserRepository } from "./variety/users/repositories/user.repository"
 
 export const userRepository = new UserRepository()
 export const authRepository = new AuthRepository()
+export const blogRepository = new BlogRepository()
+export const commentRepository = new CommentRepository()
+export const deviceRepository = new DeviceRepository()
+export const postRepository = new PostRepository()
+
 export const userQueryRepository = new UserQueryRepository()
+export const authQueryRepository = new AuthQueryRepository()
+export const blogQueryRepository = new BlogQueryRepository()
+export const commentQueryRepository = new CommentQueryRepository()
+export const deviceQueryRepository = new DeviceQueryRepository()
+export const postQueryRepository = new PostQueryRepository()
+
 export const userService = new UserService(userRepository, authRepository)
+export const authService = new AuthService(authRepository)
+export const blogService = new BlogService(blogRepository)
+export const commentService = new CommentService(commentRepository)
+export const deviceService = new DeviceService(deviceRepository)
+export const postService = new PostService(postRepository)
+
+
 export const userControllers = new UserControllers(userService, userQueryRepository)
+export const authControllers = new AuthControllers(authService, authQueryRepository)
+export const blogControllers = new BlogControllers(blogService, blogQueryRepository)
+export const commentControllers = new CommentControllers(commentService, commentQueryRepository)
+export const deviceControllers = new DeviceControllers(deviceService, deviceQueryRepository)
+export const postControllers = new PostControllers(postService, postQueryRepository)
+
